38fe6454c7ae6611fbe27372a66f7c7c6f7c5d10
Devon Terrell <axiomabsolute@gmail.com>
2017-07-08 21:35:56 -0400

    Reasonable gulp workflow
    `gulp watch` now watches the source directory and re-runs `main.js` on
    each run
    
    `gulp` builds the source directory and runs the generated `main.js` file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,33 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var exec = require('child_process').exec;
 
-gulp.task("default", function() {
+gulp.task("build", function() {
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
+});
+
+function run() {
+  return exec('node ./dist/main.js', function(err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+  });
+}
+
+gulp.task("run", function() {
+  run();
+});
+
+gulp.task('default', ['build'], function() {
+  run()
+});
+
+var watchFiles = [
+  './src/*.ts'
+];
+
+gulp.task("watch", ['default'],  function() {
+  gulp.watch('./src/*.ts', ['default']);
 });
