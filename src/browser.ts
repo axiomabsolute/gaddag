2ebe862436d3fbcbfbb348e14586a0af2a7551d0
Devon Terrell <axiomabsolute@gmail.com>
2017-07-17 20:43:22 -0400

    Ideas..

--- src/browser.ts
+++ src/browser.ts
@@ -1,66 +1,76 @@
-import { Gaddag, GaddagNode } from './gaddag';
-import { bingosSample } from './data/bingos-sample';
+import { Gaddag, GaddagNode, values } from './gaddag';
+// import { bingosSample } from './data/bingos-sample';
 // import { bingos } from './data/bingos';
-// import { words } from './data/words';
+import { words } from './data/words';
 import * as d3 from 'd3';
 
-let wordList = bingosSample.words;
+let wordList = words.words;
 var timestart = new Date().getTime();
 
+function diagonal(d: d3.HierarchyPointNode<{}>) {
+  return "M" + d.y + "," + d.x
+      + "C" + (d.parent.y + 100) + "," + d.x
+      + " " + (d.parent.y + 100) + "," + d.parent.x
+      + " " + d.parent.y + "," + d.parent.x;
+}
+
 let dag = new Gaddag();
-wordList.slice(100).forEach(w => dag.addWord(w));
+let dagSample = new Gaddag();
+wordList.slice(50,51).forEach(w => dagSample.addWord(w));
+wordList.forEach(w => dag.addWord(w));
 console.log(`Time: ${new Date().getTime() - timestart}ms`);
 console.log("\n------------------------\n");
 
 timestart = new Date().getTime();
-let dagNodes = dag.getNodes();
-let dagEdges = dag.getEdges();
+let dagNodes = dagSample.getNodes();
+let dagEdges = dagSample.getEdges();
 
 let svg = d3.select("svg"),
-  width = +svg.attr("width"),
-  height = +svg.attr("height");
+  rawWidth = 1875,
+  rawHeight = 950,
+  margin = { top: 20, right: 120, bottom: 20, left: 120 },
+  width = rawWidth - margin.right - margin.left,
+  height = rawHeight - margin.top - margin.bottom;
 
-let simulation = d3.forceSimulation()
-  .force("link", d3.forceLink().id((d: any) => d.id))
-  .force("charge", d3.forceManyBody())
-  .force("center", d3.forceCenter(width / 2, height / 2));
+svg
+  .attr('height', rawHeight)
+  .attr('width', rawWidth);
 
-let link = svg.append('g')
-  .attr('class', 'links')
-  .selectAll('line')
-  .data(dagEdges)
-  .enter().append('line')
-  .attr('stroke-width', "2");
+let frame = svg
+  .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`) ;
 
-let node = svg.append('g')
-  .attr('class', 'nodes')
-  .selectAll('circle')
-  .data(dagNodes)
-  .enter().append('circle')
-  .attr('r', 5)
-  .attr('fill', (d: any) => {
-    return d.token == 'root' ? 'red' : 'blue';
-  });
+ let hierarchyRoot = d3.hierarchy(dagSample.root, n => values(n.children));
+ let tree = d3.tree<GaddagNode>()
+  .size([height, width - 160]);
+ let treeRoot = tree(hierarchyRoot);
 
-node.append('title')
-  .text(d => `${d.id}: ${d.token}`);
+ let links = frame.selectAll('.link')
+  .data(treeRoot.descendants().slice(1))
+  .enter().append('path')
+    .attr('class', 'link')
+    .attr('d', diagonal);
+  
+let nodes = frame.selectAll('.node')
+    .data(treeRoot.descendants())
+  .enter().append('g')
+    .attr('class', d => `node ${d.children ? 'node--internal' : 'node--leaf'} ${d.data.isCompleteWord ? 'node--complete-word' : ''}` )
+    .attr('transform', d => `translate(${d.y},${d.x})`);
 
-let ticked = () => {
-  link
-    .attr("x1", function (d: any) { return d.source.x; })
-    .attr("y1", function (d: any) { return d.source.y; })
-    .attr("x2", function (d: any) { return d.target.x; })
-    .attr("y2", function (d: any) { return d.target.y; });
+nodes.append('circle')
+  .attr('r', 2.5);
 
-  node
-    .attr("cx", function (d: any) { return d.x; })
-    .attr("cy", function (d: any) { return d.y; });
-  console.log(`Time: ${new Date().getTime() - timestart}ms`);
-}
+nodes.append('text')
+  .attr('dy', 3)
+  .attr('x', d => d.children ? -8 : 8)
+  .style('text-anchor', d => d.children ? 'end' : 'start')
+  .text(d => d.data.token);
 
-simulation
-  .nodes(dagNodes)
-  .on("end", ticked);
+console.log(wordList.reduce( (p: {[length: number]: number}, n) => {
+  p[n.length] = p[n.length] || 0;
+  p[n.length] = p[n.length] + 1;
+  return p;
+}, {} ));
 
-let forceLink: any = simulation.force('link');
-forceLink.links(dagEdges);
+let anyWindow: any = window;
+anyWindow['dag'] = dag;
