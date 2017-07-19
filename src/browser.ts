2f0baabb94c845d0a063956a1dc4b7717ccef44e
Devon Terrell <axiomabsolute@gmail.com>
2017-07-18 23:05:47 -0400

    Refactor template system to be more flexible with arbitrary selectors. Comments and consistency renames

--- src/browser.ts
+++ src/browser.ts
@@ -80,18 +80,54 @@ console.log("------------------------\n");
 // console.log(dag.wordsForHandByPermutation('tion'));
 
 type TemplateIds = '#layout-exploration-slide' | '#layout-message-slide';
-type Slide<T> = { templateId: TemplateIds, markupId: string, bootstrap: (host: Element, initialState: T) => void, initialState: T };
 
+/**
+ * Represents a single slide of a presentation
+ */
+class Slide<T> 
+{
+  /**
+   * Constructor
+   * @param templateId the ID of the template element used as the base layout
+   * @param markupId the ID of the template whose top-level children are markup to inject into the layout
+   * @param bootstrap a function which takes a host element and initial state object and renders the visualization and hooks up controls
+   * @param initialState an initial state object, passed to the bootstrapping function
+   */
+  constructor(
+    public templateId: TemplateIds,
+    public markupId: string,
+    public bootstrap: (host: Element, initialState: T) => void,
+    public initialState: T
+  ) { }
+};
+
+/**
+ * A slide show based on HTML template elements
+ */
 class SlideShow {
   private host: Element;
   private _slideNumber: number;
+
+  /**
+   * Gets current slide number as 0-based index
+   * @returns active slide index
+   */
   public get slideNumber(): number {
     return this._slideNumber;
   }
+  
+  /**
+   * Gets current slide object
+   * @returns active slide
+   */
   public get slide(): Slide<any> {
     return this.slides[this._slideNumber];
   }
 
+  /**
+   * Increments the slide index if slides remain.
+   * @returns boolean value indicating whether the slide index was incremented.
+   */
   public next(): boolean {
     if (this._slideNumber < this.slides.length - 1) {
       this._slideNumber = this._slideNumber + 1;
@@ -100,6 +136,10 @@ class SlideShow {
     return false;
   }
 
+  /**
+   * Decrements the slide index if prior slides remain.
+   * @returns boolean value indicating whether the slide index was decremented.
+   */
   public previous(): boolean {
     if (this._slideNumber > 0) {
       this._slideNumber = this._slideNumber - 1;
@@ -108,6 +148,11 @@ class SlideShow {
     return false;
   }
 
+  /**
+   * Jumps to a specified slide.
+   * @param index slide index to jump to
+   * @returns boolean value indicating whether the slide index was mutated
+   */
   private goToPage(index: number): boolean {
     if (index >= 0 && index < this.slides.length) {
       this._slideNumber = index;
@@ -116,6 +161,17 @@ class SlideShow {
     return false;
   }
 
+  /**
+   * Render the current slide.
+   * 
+   * Clear out the current slide's markup from the page.
+   * Queries the document for the active slide's layout template and creates a clone.
+   * Queries the document for the active slide's markup template and creates a clone.
+   * For each child element in the cloned markup template, look for an element with the same class
+   * in the cloned layout template and replace the contents with the markup contents.
+   * Execute the active slide's bootstrap function on the cloned layout template.
+   * Inject the cloned layout template into the page.
+   */
   private renderSlide() {
     let currentSlide = this.host.querySelector('.show');
     currentSlide.innerHTML = '';
@@ -128,12 +184,10 @@ class SlideShow {
       let newSlideMarkupNode: Element = document.importNode(newSlideMarkupTemplate.content, true);
       for (var i = 0; i < newSlideMarkupNode.children.length; i++) {
         let child = newSlideMarkupNode.children[i];
-        let childClasses = child.className.split(' ');
-        childClasses.forEach(c => {
-          if (c.length == 0) { return; }
-          let node = clone.querySelector(`.${c}`);
+        if (child.hasAttribute('data-selector')) {
+          let node = clone.querySelector(`${child.getAttribute('data-selector')}`);
           node.innerHTML = child.innerHTML;
-        });
+        }
       }
     }
 
@@ -142,19 +196,31 @@ class SlideShow {
     currentSlide.appendChild(clone);
   }
 
-  private renderPageControls() {
+  /**
+   * Updates the show control state and renders active slide. 
+   */
+  private renderShowControls() {
     d3.selectAll('.page-dot')
       .classed('page-dot--active', (d, i) => i == this._slideNumber);
-    this.renderSlide();
   }
 
+  /**
+   * 
+   * @param slides array of slides, whose order determines the order of the show
+   * @param initialSlide index of initial active slide
+   * @param hostSelector selector for DOM element hosting the show
+   */
   constructor(public slides: Slide<any>[], initialSlide: number = 0, hostSelector: string = ".slide-show") {
+    // Set initial slide, defaults to 0 if out of bounds
     this._slideNumber = slides[initialSlide] ? initialSlide : 0;
 
+    // Find the host
     this.host = document.querySelector(hostSelector);
 
+    // Render the current slide
     this.renderSlide();
 
+    // Render the show controls and attach click event handlers
     d3.select(this.host).select('.page-dots')
       .selectAll('.page-dot')
       .data(slides).enter()
@@ -165,46 +231,57 @@ class SlideShow {
       .text((d, i) => i + 1)
       .on('click', (d, i) => {
         this.goToPage(i);
-        this.renderPageControls();
+        this.renderShowControls();
+        this.renderSlide();
       });
 
+    // Attach next slide handler
     d3.select('.next-slide').on('click', () => {
       let result = this.next();
       if (!result) { return; }
       this.renderSlide();
-      this.renderPageControls();
+      this.renderShowControls();
+      this.renderSlide();
     });
+    // Attach previous slide handler
     d3.select('.prev-slide').on('click', () => {
       let result = this.previous();
       if (!result) { return; }
       this.renderSlide();
-      this.renderPageControls();
+      this.renderShowControls();
+      this.renderSlide();
     });
   }
 }
 
-function populateExplorationSlide(host: Element, title: string, messages: string[], controls: Element, explorations: string[]) {
-  d3.select(host).select('.slide-title').text(title);
-  d3.select(host).select('.slide-text')
-    .selectAll('p').data(messages)
-    .enter().append('p')
-    .text(d => d);
-}
-
+/**
+ * Placeholder for future slide types
+ * @param host host element to inject into
+ */
 function helloWorldSlide(host: Element) {
   d3.select(host).select('.slide-title').text("Hello");
   d3.select(host).select('.slide-text').text("World!");
 }
 
-let slides: Slide<any>[] = [
-  { templateId: '#layout-exploration-slide', markupId: '#clabbers-slide', bootstrap: clabbersSlide, initialState: clabbersSlideInitialState },
-  { templateId: '#layout-message-slide', markupId: null, bootstrap: helloWorldSlide, initialState: {} },
-];
-
+/**
+ * Set initial page based on URL hash location
+ */
 let initialPage = 0;
 if (window.location.hash) {
   let splits = window.location.hash.split('page-');
   initialPage = splits.length > 1 ? parseInt(splits[1]) : 0;
 }
 
+/**
+ * Array of slides to setup show.
+ * Modify this array to add additional slides to the show.
+ */
+let slides: Slide<any>[] = [
+  { templateId: '#layout-exploration-slide', markupId: '#clabbers-slide', bootstrap: clabbersSlide, initialState: clabbersSlideInitialState },
+  { templateId: '#layout-message-slide', markupId: null, bootstrap: helloWorldSlide, initialState: {} },
+];
+
+/**
+ * Start the show!
+ */
 let show = new SlideShow(slides, initialPage);
