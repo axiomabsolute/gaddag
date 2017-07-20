65bfd079ebee1b5d1d90b6e5c0d404db48001fe3
Devon Terrell <axiomabsolute@gmail.com>
2017-07-20 13:29:16 -0400

    Add a legend and some default placeholders for exploration slide layout

--- src/slides/two-letter.ts
+++ src/slides/two-letter.ts
@@ -2,22 +2,33 @@ import { Dictionary, Gaddag, keyValuePairs, values } from '../gaddag';
 
 import * as d3 from 'd3';
 
-function update(frame: d3.Selection<Element | d3.EnterElement | Document | Window, {}, null, undefined>, data: Dictionary<boolean>, cellSize: number) {
+function update(
+  frame: d3.Selection<Element | d3.EnterElement | Document | Window, {}, null, undefined>, data: Dictionary<number>,
+  cellSize: number,
+  isWordColor: string,
+  nonWordColor: string
+) {
   let baseCharCode = 'a'.charCodeAt(0);
   let cellPadding = 3;
   let cellRadius = Math.floor((cellSize - (cellPadding * 2))/2);
-  frame.selectAll('.two-letter-word')
-    .data(keyValuePairs(data)).enter()
+  let wordNodes = frame.selectAll('.two-letter-word')
+    .data(keyValuePairs(data))
+  
+  wordNodes.enter()
     .append('circle')
+    .merge(wordNodes)
     .attr('class', 'two-letter-word')
     .attr('title', d => d.key)
     .attr('r', cellRadius)
     .attr('dx', cellPadding)
     .attr('dy', cellPadding)
-    .attr('fill', d => d.value ? 'red' : 'black')
+    .attr('fill', d => d.value ? isWordColor : nonWordColor)
     .attr('transform', (d, i) => {
       return `translate(${(d.key.charCodeAt(1) - baseCharCode) * cellSize},${(d.key.charCodeAt(0) - baseCharCode) * cellSize})`;
-    })
+    });
+  
+  wordNodes.exit().remove();
+
 }
 
 export class InitialState{
@@ -35,10 +46,11 @@ export function bootstrap(host: Element, initialState: InitialState) {
     width = rawWidth - margin.right - margin.left,
     height = rawHeight - margin.top - margin.bottom;
 
-  let frame = svg
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
-  
+  let cellSize = 20,
+      axisMargin = 20,
+      isWordColor = 'red',
+      nonWordColor = 'black';
+
   let letters: string[] = [];
   let i = 'a'.charCodeAt(0);
   let j = 'z'.charCodeAt(0);
@@ -46,33 +58,35 @@ export function bootstrap(host: Element, initialState: InitialState) {
     letters.push(String.fromCharCode(i));
   }
 
-  let data = new Dictionary<boolean>();
+  let data = new Dictionary<number>();
   letters.forEach( l1 => letters.forEach(l2 => {
     let key = `${l1}${l2}`;
-    data[key] = twoLetterWords[key];
+    data[key] = twoLetterWords[key] ? 1 : 0;
   }));
 
-  let cellSize = 20;
-
+  let frame = svg
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+  
   let topAxis = frame
     .append('g')
     .attr('class', 'top-axis')
-    .attr('transform', `translate(${cellSize + 20},0)`);
+    .attr('transform', `translate(${cellSize + axisMargin},0)`);
 
   let leftAxis = frame
     .append('g')
     .attr('class', 'left-axis')
-    .attr('transform', `translate(0,${cellSize + 20})`);
+    .attr('transform', `translate(0,${cellSize + axisMargin})`);
   
   let gridArea = frame
     .append('g')
     .attr('class', 'grid')
-    .attr('transform', `translate(${cellSize + 5 + 20},${cellSize - 5 + 20})`);
+    .attr('transform', `translate(${cellSize + 5 + axisMargin},${cellSize - 5 + axisMargin})`);
   
   topAxis.selectAll('.letter')
     .data(letters).enter()
     .append('text')
-    .attr('transform', (d, i) => `translate(${i*cellSize},20)`)
+    .attr('transform', (d, i) => `translate(${i*cellSize},${axisMargin})`)
     .text(d => d);
   
   topAxis
@@ -86,7 +100,7 @@ export function bootstrap(host: Element, initialState: InitialState) {
   leftAxis.selectAll('.letter')
     .data(letters).enter()
     .append('text')
-    .attr('transform', (d, i) => `translate(20,${i*cellSize})`)
+    .attr('transform', (d, i) => `translate(${axisMargin},${i*cellSize})`)
     .text(d => d);
   
   leftAxis
@@ -97,8 +111,42 @@ export function bootstrap(host: Element, initialState: InitialState) {
     .append('text')
       .text('First letter');
   
+  let legend = frame.append('g')
+    .attr('transform', `translate(${26*cellSize + (2 * (axisMargin + cellSize))},${axisMargin+cellSize})`)
+    .attr('class', 'legend');
+
+  legend.append('g')
+    .attr('text-anchor', 'middle')
+      .append('text')
+        .text('Legend:');
+
+  let positiveKey = legend.append('g')
+    .attr('transform', `translate(0,${cellSize})`)
+    .attr('class', 'is-word-key');
+  
+  positiveKey.append('circle')
+    .attr('r', 10)
+    .attr('fill', isWordColor);
+  
+  positiveKey.append('text')
+    .text('- is a word')
+    .attr('transform', `translate(${cellSize + 5},5)`)
+  
+  let negativeKey = legend.append('g')
+    .attr('transform', `translate(0,${3*cellSize})`)
+    .attr('class', 'is-not-word-key');
+
+  negativeKey.append('circle')
+    .attr('r', 10)
+    .attr('fill', nonWordColor);
+  
+  negativeKey.append('text')
+    .text('- is not a word')
+    .attr('transform', `translate(${cellSize + 5},5)`)
+  
+  
   host.querySelector('.two-letter-plays')
     .innerHTML = `${values(data).filter(f => f).length}`;
   
-  update(gridArea, data, cellSize);
+  update(gridArea, data, cellSize, isWordColor, nonWordColor);
 }
