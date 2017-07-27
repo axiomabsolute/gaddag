c9cd866807fb84a750ecee048c6ad16c1e5cfda5
Devon Terrell <axiomabsolute@gmail.com>
2017-07-27 00:16:38 -0400

    Use wordList for patterns slide

--- src/slides/patterns.slide.ts
+++ src/slides/patterns.slide.ts
@@ -28,12 +28,19 @@ export function bootstrap(host: Element, initialState: InitialState) {
   let data: any[] = [];
 
   initialState.dataLoaded.then((wordList: string[]) => {
-    let wordsWithFix = initialState.dag.wordsContaining('ing');
-    let wordsContaining = initialState.dag.wordsForHand('ing???????');
+    // let wordsWithFix = initialState.dag.wordsContaining('ing').filter(w => w.length <= 7);
+    // let wordsContaining = initialState.dag.wordsForHand('ing????');
 
-    let anyWindow: any = window;
-    anyWindow['wordList'] = wordList;
+    // console.log(wordsWithFix.length);
+    // console.log(wordsContaining.filter(w => /i/.test(w) && /n/.test(w) && /g/.test(w)).length);
+    // console.log("-----------------");
+
+    let sevenLetterOrLessWords = wordList.filter(w => w.length <= 7);
+    let ingWords = sevenLetterOrLessWords.filter(w => /ing/.test(w))
+    let wordsWithING = sevenLetterOrLessWords.filter(w => /i/.test(w) && /n/.test(w) && /g/.test(w))
     
+    // Partition - words containing "ing" vs "i", "n", and "g"
+
     update(frame, data, width, height);
   });
 
