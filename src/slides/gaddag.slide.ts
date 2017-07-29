8546e1c2f2507477858c85f2cd064b8062a937fc
Devon Terrell <axiomabsolute@gmail.com>
2017-07-29 17:47:49 -0400

    Gaddag slide up and running

--- src/slides/gaddag.slide.ts
+++ src/slides/gaddag.slide.ts
@@ -1,14 +1,19 @@
-import { hierarchy, select, tree } from 'd3';
-import { Gaddag, GaddagEdge, GaddagNode, values } from '../gaddag';
+import * as d3 from 'd3';
+import { Dictionary, flatten, Gaddag, GaddagEdge, GaddagNode, values } from '../gaddag';
 import { Fix } from '../browser';
 
-let d3 = { hierarchy, select, tree };
+function diagonal(source: VisualNode, target: VisualNode) {
+  return "M" + target.x + "," + target.y
+      + "C" + (source.x + 100) + "," + target.y
+      + " " + (source.x + 100) + "," + source.y
+      + " " + source.x + "," + source.y;
+}
 
-function diagonal(d: d3.HierarchyPointNode<{}>) {
-  return "M" + d.y + "," + d.x
-      + "C" + (d.parent.y + 100) + "," + d.x
-      + " " + (d.parent.y + 100) + "," + d.parent.x
-      + " " + d.parent.y + "," + d.parent.x;
+class VisualNode {
+  public meta: Dictionary<any>;
+  constructor(public node: GaddagNode, public x: number, public y: number) {
+    this.meta = {};
+  }
 }
 
 function update(
@@ -16,36 +21,72 @@ function update(
   width: number,
   height: number,
   dag: Gaddag,
-  root: GaddagNode,
-  dagEdges: GaddagEdge[],
-  dagNodes: GaddagNode[],
   pattern: string
 ) {
-    let hierarchyRoot = d3.hierarchy(root, n => values(n.children));
-    let tree = d3.tree<GaddagNode>()
-      .size([height, width - 160]);
-    let treeRoot = tree(hierarchyRoot);
+    let dagEdges = dag.getEdges();
+    let root = dag.root;
+
+    let nodesByDepth = [[root]].concat(values(dag.getNodesByDepth()));
+    let depth = nodesByDepth.length;
+    let breadth = d3.max(nodesByDepth.map(t => t.length));
+
+    let xScale = d3.scaleLinear()
+      .domain([0, depth])
+      .range([0, width]);
+    
+    let yScale = d3.scaleLinear()
+      .domain([0, breadth])
+      .range([height, 0]);
+
+    let nodeData = flatten(nodesByDepth.map( (tier, i) => tier.map((node, j) => new VisualNode(node, xScale(i), yScale(j)))));
+    let nodeLookup = Dictionary.ToLookup<VisualNode, VisualNode>(nodeData, vn => '' + vn.node.id, vn => vn);
 
     let links = frame.selectAll('.link')
-      .data(treeRoot.descendants().slice(1))
+      .data(dagEdges);
+    
+    links
       .enter().append('path')
-        .attr('class', 'link')
-        .attr('d', diagonal);
+      .merge(links)
+      .attr('class', 'link')
+      .attr('d', d => diagonal( nodeLookup[''+d.source], nodeLookup[''+d.target] ));
 
     let nodes = frame.selectAll('.node')
-        .data(treeRoot.descendants())
+      .data(nodeData);
+
+    let nodeGroups = nodes
       .enter().append('g')
-        .attr('class', d => `node ${d.children ? 'node--internal' : 'node--leaf'} ${d.data.isCompleteWord ? 'node--complete-word' : ''}` )
-        .attr('transform', d => `translate(${d.y},${d.x})`);
-
-    nodes.append('circle')
-      .attr('r', 2.5);
-
-    nodes.append('text')
+      .merge(nodes)
+        .attr('class', d => {
+          if (d.node.token === Gaddag.TurnToken) {
+            return 'node dag-node node--turn';
+          }
+          if (d.node.isCompleteWord) {
+            return 'node dag-node node--complete-word'
+          }
+          return 'node dag-node';
+        })
+        .attr('transform', d => `translate(${d.x},${d.y})`);
+    
+    nodeGroups
+      .append('circle')
+        .attr('r', '8')
+        .attr('fill', (d) => {
+          if (d.node.token === Gaddag.TurnToken) {
+            return d3.schemeCategory10[1];
+          }
+          if (d.node.isCompleteWord) {
+            return d3.schemeCategory10[2];
+          }
+          return d3.schemeCategory10[0];
+        });
+    
+    nodeGroups
+      .append('text')
       .attr('dy', 3)
-      .attr('x', d => d.children ? -8 : 8)
-      .style('text-anchor', d => d.children ? 'end' : 'start')
-      .text(d => d.data.token);
+      .attr('x', d => d.node.children ? -12 : 12)
+      .style('text-anchor', d => d.node.children ? 'end' : 'start')
+      .text(d => d.node.token);
+      
 
     let anyWindow: any = window;
     anyWindow['dag'] = dag;
@@ -56,10 +97,11 @@ export class InitialState{
 }
 
 export function bootstrap(host: Element, initialState: InitialState) {
+
   let svg = d3.select(host).select("svg"),
-    rawWidth = 1875,
-    rawHeight = 950,
-    margin = { top: 20, right: 120, bottom: 20, left: 120 },
+    rawWidth = +svg.attr('width'),
+    rawHeight = +svg.attr('height'),
+    margin = { top: 60, right: 120, bottom: 120, left: 120 },
     width = rawWidth - margin.right - margin.left,
     height = rawHeight - margin.top - margin.bottom;
 
@@ -72,11 +114,8 @@ export function bootstrap(host: Element, initialState: InitialState) {
       .attr('transform', `translate(${margin.left},${margin.top})`) ;
 
   initialState.dataLoaded.then(() => {
-    let dagNodes = initialState.dag.getNodes();
-    let dagEdges = initialState.dag.getEdges();
-    let root = initialState.dag.root;
 
-    update(frame, width, height, initialState.dag, root, dagEdges, dagNodes, '')
+    update(frame, width, height, initialState.dag, '')
   })
 
   // console.log(dag.wordsForHandByPermutation('tion'));
